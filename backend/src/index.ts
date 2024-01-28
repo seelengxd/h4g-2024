f0562b237d097b731805edfeacaa3efce95aa97d
see leng <seelengxd@gmail.com>
2024-01-28 08:16:01 +0800

    feat: init prisma and middleware

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,18 @@
 import express, { Express, Request, Response } from "express";
+import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
+import cors from "cors";
+import logger from "morgan";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+app.use(logger("tiny"));
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");

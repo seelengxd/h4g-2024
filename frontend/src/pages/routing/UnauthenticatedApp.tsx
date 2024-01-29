9cea5e7db406b52770e01276709d125ddb19a7b8
see leng <seelengxd@gmail.com>
2024-01-30 01:11:10 +0800

    fix: fix routing

--- frontend/src/pages/routing/UnauthenticatedApp.tsx
+++ frontend/src/pages/routing/UnauthenticatedApp.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import LogInForm from "../auth/LogInForm";
 import SignUpForm from "../auth/SignupForm";
 
@@ -8,13 +8,9 @@ const UnauthenticatedApp: React.FC = () => {
       <Route path="/" element={<LogInForm />} />
       <Route path="/signup" element={<SignUpForm />} />
 
-      //Default pages to sign in form if user not logged in
-      <Route path="/organisations" element={<SignUpForm />} />
-      <Route path="/volunteers" element={<SignUpForm />} />
-      <Route path="/activities" element={<SignUpForm />} />
-      <Route path="/forum" element={<SignUpForm />} />
+      {/* Default pages to sign in form if user not logged in */}
+      <Route path="*" element={<Navigate to={"/"} />} />
     </Routes>
-
   );
 };
 

204af26199fad545359c97ab81fb54a9b55f2f8b
see leng <seelengxd@gmail.com>
2024-02-03 13:48:42 +0800

    feat(backend): add submissions api

--- backend/src/routes/base.ts
+++ backend/src/routes/base.ts
@@ -3,6 +3,7 @@ import authRouter from "./auth";
 import organisationsRouter from "./organisations";
 import activitiesRouter from "./activities";
 import enrollmentFormsRouter from "./enrollmentForms";
+import submissionsRouter from "./submissions";
 
 const apiRouter = Router();
 
@@ -10,4 +11,5 @@ apiRouter.use("/auth", authRouter);
 apiRouter.use("/organisations", organisationsRouter);
 apiRouter.use("/activities", activitiesRouter);
 apiRouter.use("/enrollment-forms", enrollmentFormsRouter);
+apiRouter.use("/submissions", submissionsRouter);
 export default apiRouter;

435cb1964f76c615e8bd70e90d40d85afe72e568
chloelim <chloelim22@gmail.com>
2024-02-04 04:12:16 +0800

    refactor: label component

--- frontend/src/components/forms/Label.tsx
+++ frontend/src/components/forms/Label.tsx
@@ -1,12 +1,14 @@
 interface Props extends React.PropsWithChildren {
   htmlFor: string;
+  textSize?: string;
+  mb?: number;
 }
 
-const Label: React.FC<Props> = ({ htmlFor, children }) => {
+const Label: React.FC<Props> = ({ htmlFor, textSize = 'text-sm', mb = 0, children }) => {
   return (
     <label
       htmlFor={htmlFor}
-      className="block text-sm font-medium text-gray-900 leading-6"
+      className={`block ${textSize} font-medium text-gray-900 leading-6 mb-${mb}`}
     >
       {children}
     </label>

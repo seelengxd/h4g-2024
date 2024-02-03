b4f766643775a3130ebb817bfe7733d78c285e78
chloelim <chloelim22@gmail.com>
2024-02-03 22:38:05 +0800

    feat: extend button styling

--- frontend/src/components/buttons/Button.tsx
+++ frontend/src/components/buttons/Button.tsx
@@ -3,6 +3,13 @@ import { PropsWithChildren } from "react";
 interface Props extends PropsWithChildren {
   type?: "button" | "submit" | "reset";
   fullWidth?: boolean;
+  bgColor?: string;
+  textColor?: string;
+  roundness?: string;
+  px?: number;
+  py?: number;
+  outlined?: boolean;
+  outlineColor?: string;
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
 }
 
@@ -10,14 +17,21 @@ const Button: React.FC<Props> = ({
   type = "button",
   children,
   fullWidth = false,
+  bgColor = 'primary',
+  textColor = 'text-white',
+  roundness = 'full',
+  px = 3,
+  py = 3,
+  outlined = false,
+  outlineColor = 'border-black',
   onClick,
 }) => {
   return (
     <button
       type={type}
       className={
-        "flex justify-center items-center rounded-full bg-primary-600 px-3 py-3 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-primary-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" +
-        (fullWidth ? " w-full" : "")
+        `flex justify-center items-center rounded-${roundness} bg-${bgColor}-600 px-${px} py-${py} text-sm font-semibold leading-6 ${textColor} shadow-sm hover:bg-${bgColor}-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-${bgColor}-600` +
+        (fullWidth ? " w-full" : "") + (outlined ? ` border-2 ${outlineColor}` : "")
       }
       onClick={onClick}
     >

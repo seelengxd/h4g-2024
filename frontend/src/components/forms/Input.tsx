02b549f2919df5475dcf656a5c060dcfbcba357e
see leng <seelengxd@gmail.com>
2024-02-06 20:09:27 +0800

    feat: revert color back to grey

--- frontend/src/components/forms/Input.tsx
+++ frontend/src/components/forms/Input.tsx
@@ -41,11 +41,14 @@ const Input: React.FC<InputProps & React.HTMLAttributes<HTMLInputElement>> = (
         {...(required ? { required: true } : {})}
         {...(value ? { value } : {})}
         onChange={onChange}
-        className={"block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-primary-200 rounded-2xl border-1 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-primary-600 peer" + (hidden ? " hidden" : "")}
+        className={
+          "block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900  rounded-2xl border-1 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-primary-600 peer" +
+          (hidden ? " hidden" : "")
+        }
       />
       <label
         htmlFor={name}
-        className="absolute text-sm text-gray-500 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white peer-focus:bg-white px-2 peer-focus:px-2 peer-placeholder-shown:bg-primary-200 peer-focus:text-primary-800 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1"
+        className="absolute text-sm text-gray-500 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white peer-focus:bg-white px-2 peer-focus:px-2 peer-focus:text-primary-800 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1"
       >
         {label}
       </label>
